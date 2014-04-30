7238bce1b7e3dfd72207df449b0805a5a605d013
priyaganti <ganti.priya@gmail.com>
2014-04-30 16:54:00 -0700

    add zipcode amd membership validation to create member page and remove from search member page.

--- server_controller.js
+++ server_controller.js
@@ -11,7 +11,7 @@ var title = 'EJS template with Node.JS';
 var data = 'Data from node';
 var mysql_connection = require('mysql');
 
-// ----MYSQL Connection----//
+//----MYSQL Connection----//
 var connection = mysql_connection.createConnection({
 	host : 'localhost',
 	user : 'root',
@@ -158,130 +158,120 @@ app.post('/searchCustomer', function(req, res) {
 });
 
 app
-		.post(
-				'/addNewMember',
-				function(req, res) {
-					if (connection) {
-						// Catching parameters and check if they are available
-						// or not
-						var memType = req.param("member");
+.post('/addNewMember',function(req, res) {
+	if (connection) {
+		// Catching parameters and check if they are available
+		// or not
+		var memType = req.param("member");
 
-						var firstName = req.param("firstName");
+		var firstName = req.param("firstName");
 
-						var lastName = req.param("lastName");
+		var lastName = req.param("lastName");
 
-						var membershipNo = req.param("memNum")
+		var membershipNo = req.param("memNum")
 
-						var address = req.param("address");
+		var address = req.param("address");
 
-						var city = req.param("city");
+		var city = req.param("city");
 
-						var state = req.param("state");
+		var state = req.param("state");
 
-						var zipCode = req.param("zipCode");
+		var zipCode = req.param("zipCode");
 
-						// create the query for each one
-						var query = 'INSERT INTO `videolibrarymanagement`.`customers` (`id`, `membershipno`, `fname`, `lname`, `address`, `city`, `state`, `zipcode`, `membertype`, `amount`) VALUES (NULL, '
-								+ connection.escape(membershipNo)
-								+ ','
-								+ connection.escape(firstName)
-								+ ','
-								+ connection.escape(lastName)
-								+ ','
-								+ connection.escape(address)
-								+ ','
-								+ connection.escape(city)
-								+ ','
-								+ connection.escape(state)
-								+ ','
-								+ connection.escape(zipCode)
-								+ ','
-								+ connection.escape(memType) + ',\'\')';
+		// create the query for each one
+		var query = 'INSERT INTO `videolibrarymanagement`.`customers` (`id`, `membershipno`, `fname`, `lname`, `address`, `city`, `state`, `zipcode`, `membertype`, `amount`) VALUES (NULL, '
+			+ connection.escape(membershipNo)
+			+ ','
+			+ connection.escape(firstName)
+			+ ','
+			+ connection.escape(lastName)
+			+ ','
+			+ connection.escape(address)
+			+ ','
+			+ connection.escape(city)
+			+ ','
+			+ connection.escape(state)
+			+ ','
+			+ connection.escape(zipCode)
+			+ ','
+			+ connection.escape(memType) + ',\'\')';
 
-						console.log("Add a new member " + " " + memType + " "
-								+ firstName + " " + lastName + " "
-								+ membershipNo + " " + address + " " + city
-								+ " " + state + " " + zipCode);
-						console.log("Query : " + query);
-						connection
-								.query(
-										query,
-										function(err, members) {
-											if (err) {
-												console
-														.log("Can't add new member to DB");
-												return res
-														.send('Failed to created a new member');
-											} else {
-												console.log(members);
-												return res
-														.send('Successfully created a new member');
-											}
+		console.log("Add a new member " + " " + memType + " "
+				+ firstName + " " + lastName + " "
+				+ membershipNo + " " + address + " " + city
+				+ " " + state + " " + zipCode);
+		console.log("Query : " + query);
+		connection.query(query,function(err, members) {
+			if (err) {
+				console
+				.log("Can't add new member to DB");
+				return res
+				.send('Failed to created a new member');
+			} else {
+				console.log(members);
+				return res
+				.send('Successfully created a new member');
+			}
 
-										});
-					}
-					return;
+		});
+	}
+	return;
 
-				});
+});
 
-app
-		.post(
-				'/addNewMovie',
-				function(req, res) {
-					if (connection) {
-						// Catching parameters and check if they are available
-						// or not
-						var movieName = req.param("movieName");
+app.post('/addNewMovie',
+		function(req, res) {
+	if (connection) {
+		// Catching parameters and check if they are available
+		// or not
+		var movieName = req.param("movieName");
 
-						var movieBanner = req.param("movieBanner");
+		var movieBanner = req.param("movieBanner");
 
-						var releaseDate = req.param("rdate");
+		var releaseDate = req.param("rdate");
 
-						var category = req.param("category");
+		var category = req.param("category");
 
-						var availableCopies = req.param("quantity");
+		var availableCopies = req.param("quantity");
 
-						var rentAmount = req.param("rentAmount");
+		var rentAmount = req.param("rentAmount");
 
-						// create the query for each one
-						var query = 'INSERT INTO `videolibrarymanagement`.`movies` (`id`, `name`, `bannerName`, `releaseDate`, `category`, `rentAmount`, `availableCopies`) VALUES (NULL, '
-								+ connection.escape(movieName)
-								+ ','
-								+ connection.escape(movieBanner)
-								+ ','
-								+ connection.escape(releaseDate)
-								+ ','
-								+ connection.escape(category)
-								+ ','
-								+ connection.escape(rentAmount)
-								+ ','
-								+ connection.escape(availableCopies) + ' )';
+		// create the query for each one
+		var query = 'INSERT INTO `videolibrarymanagement`.`movies` (`id`, `name`, `bannerName`, `releaseDate`, `category`, `rentAmount`, `availableCopies`) VALUES (NULL, '
+			+ connection.escape(movieName)
+			+ ','
+			+ connection.escape(movieBanner)
+			+ ','
+			+ connection.escape(releaseDate)
+			+ ','
+			+ connection.escape(category)
+			+ ','
+			+ connection.escape(rentAmount)
+			+ ','
+			+ connection.escape(availableCopies) + ' )';
 
-						console.log("Add a new movie " + " " + movieName + " "
-								+ movieBanner + " " + releaseDate + " "
-								+ category + " " + rentAmount + " "
-								+ availableCopies);
-						console.log("Query : " + query);
-						connection
-								.query(
-										query,
-										function(err, movies) {
-											if (err) {
-												console
-														.log("Can't add new movie to DB");
-												return res
-														.send('Failed to create a new movie');
-											} else {
-												console.log(movies);
-												return res
-														.send('Successfully add a new movie');
-											}
+		console.log("Add a new movie " + " " + movieName + " "
+				+ movieBanner + " " + releaseDate + " "
+				+ category + " " + rentAmount + " "
+				+ availableCopies);
+		console.log("Query : " + query);
+		connection.query(query,function(err, movies) {
+			if (err) {
+				console
+				.log("Can't add new movie to DB");
+				return res
+				.send('Failed to create a new movie');
+			} else {
+				console.log(movies);
+				return res
+				.send('Successfully add a new movie');
+			}
 
-										});
-					}
-					return;
+		});
+	}
+	return;
 
-				});
+});
 
 app.post('/searchMovie', function(req, res) {
 
@@ -301,80 +291,74 @@ app.post('/searchMovie', function(req, res) {
 	});
 
 });
-// -----------Update a Member-----------//
-app
-		.post(
-				'/updateMember',
-				function(req, res) {
-					if (connection) {
-						// Catching parameters and check if they are available
-						// or not
-						var memType = req.param("member");
+//-----------Update a Member-----------//
+app.post('/updateMember',function(req, res) {
+	if (connection) {
+		// Catching parameters and check if they are available
+		// or not
+		var memType = req.param("member");
 
-						var firstName = req.param("firstName");
+		var firstName = req.param("firstName");
 
-						var lastName = req.param("lastName");
+		var lastName = req.param("lastName");
 
-						var membershipNo = req.param("memNum")
+		var membershipNo = req.param("memNum")
 
-						var address = req.param("address");
+		var address = req.param("address");
 
-						var city = req.param("city");
+		var city = req.param("city");
 
-						var state = req.param("state");
+		var state = req.param("state");
 
-						var zipCode = req.param("zipCode");
+		var zipCode = req.param("zipCode");
 
-						var status = req.param("status");
-						var oldMembershipNo = req.param("oldMembershipNo");
+		var status = req.param("status");
+		var oldMembershipNo = req.param("oldMembershipNo");
 
-						// create the query for each one
-						var query = 'UPDATE `videolibrarymanagement`.`customers` SET `membershipno` ='
-								+ connection.escape(membershipNo)
-								+ ',`fname` = '
-								+ connection.escape(firstName)
-								+ ',`lname` = '
-								+ connection.escape(lastName)
-								+ ',`address` = '
-								+ connection.escape(address)
-								+ ',	`city` ='
-								+ connection.escape(city)
-								+ ',`state` = '
-								+ connection.escape(state)
-								+ ',`zipcode` = '
-								+ connection.escape(zipCode)
-								+ ',`membertype` = '
-								+ connection.escape(memType)
-								+ ',`status` = '
-								+ connection.escape(status)
-								+ 'WHERE `customers`.`membershipno` ='
-								+ connection.escape(oldMembershipNo);
+		// create the query for each one
+		var query = 'UPDATE `videolibrarymanagement`.`customers` SET `membershipno` ='
+			+ connection.escape(membershipNo)
+			+ ',`fname` = '
+			+ connection.escape(firstName)
+			+ ',`lname` = '
+			+ connection.escape(lastName)
+			+ ',`address` = '
+			+ connection.escape(address)
+			+ ',	`city` ='
+			+ connection.escape(city)
+			+ ',`state` = '
+			+ connection.escape(state)
+			+ ',`zipcode` = '
+			+ connection.escape(zipCode)
+			+ ',`membertype` = '
+			+ connection.escape(memType)
+			+ ',`status` = '
+			+ connection.escape(status)
+			+ 'WHERE `customers`.`membershipno` ='
+			+ connection.escape(oldMembershipNo);
 
-						console.log("Add a new member " + " " + memType + " "
-								+ firstName + " " + lastName + " "
-								+ membershipNo + " " + address + " " + city
-								+ " " + state + " " + zipCode + " " + status);
-						console.log("Query : " + query);
-						connection
-								.query(
-										query,
-										function(err, members) {
-											if (err) {
-												console
-														.log("Can't edit the member in DB");
-												return res
-														.send('Failed to edit the member');
-											} else {
-												console.log(members);
-												return res
-														.send("Successfully edited" );
-											}
+		console.log("Add a new member " + " " + memType + " "
+				+ firstName + " " + lastName + " "
+				+ membershipNo + " " + address + " " + city
+				+ " " + state + " " + zipCode + " " + status);
+		console.log("Query : " + query);
+		connection.query(query,function(err, members) {
+			if (err) {
+				console
+				.log("Can't edit the member in DB");
+				return res
+				.send('Failed to edit the member');
+			} else {
+				console.log(members);
+				return res
+				.send("Successfully edited" );
+			}
 
-										});
-					}
+		});
+	}
 
-				});
-// -----------Edit a Member-----------//
+});
+//-----------Edit a Member-----------//
 app.post('/editMember', function(req, res) {
 	var userInfo = req.param("member");
 	console.log("Edit member: " + userInfo);
@@ -384,7 +368,7 @@ app.post('/editMember', function(req, res) {
 
 		// create the query for each one
 		var query = "Select * From customers Where membershipno ="
-				+ connection.escape(userInfo);
+			+ connection.escape(userInfo);
 
 		console.log("Edit a member " + "Query : " + query);
 		connection.query(query, function(err, members) {
@@ -415,28 +399,25 @@ app.post('/editMember', function(req, res) {
 	// res.send("Successfully edit the member" + userInfo);
 
 });
-// -----------Delete a Member-----------//
-app
-		.post(
-				'/deleteMember',
-				function(req, res) {
-					var memberNo = req.param("member");
-					console.log("Edit member: " + memberNo);
-					var query = 'UPDATE  `videolibrarymanagement`.`customers` SET  `status` =  \'inactive\' WHERE  `customers`.`membershipno` = '
-							+ connection.escape(memberNo);
-					connection.query(query, function(err, result) {
-						if (!err) {
-							console.log("users is inactive now: " + result);
-						} else {
-							console.log("error can't edit user's status: "
-									+ err);
-						}
+//-----------Delete a Member-----------//
+app.post('/deleteMember',function(req, res) {
+			var memberNo = req.param("member");
+			console.log("Edit member: " + memberNo);
+			var query = 'UPDATE  `videolibrarymanagement`.`customers` SET  `status` =  \'inactive\' WHERE  `customers`.`membershipno` = '
+				+ connection.escape(memberNo);
+			connection.query(query, function(err, result) {
+				if (!err) {
+					console.log("users is inactive now: " + result);
+				} else {
+					console.log("error can't edit user's status: "
+							+ err);
+				}
 
-					});
-					res.send("Successfully deleted the member" + memberNo);
+			});
+			res.send("Successfully deleted the member" + memberNo);
 
-				});
-// -----------Issue a Movie-----------//
+		});
+//-----------Issue a Movie-----------//
 app.post('/issueMovie', function(req, res) {
 	var userInfo = req.param("member");
 	console.log("Edit member: " + userInfo);
@@ -444,7 +425,7 @@ app.post('/issueMovie', function(req, res) {
 	res.render('issuedMovies.ejs');
 
 });
-// -----------Return a Movie-----------//
+//-----------Return a Movie-----------//
 app.post('/returnMovie', function(req, res) {
 	var userInfo = req.param("member");
 	console.log("Edit member: " + userInfo);
@@ -485,35 +466,35 @@ app.post('/DisplayAllMoviesJSON', function(req, res) {
 			movieBanner = movieBanner + "%";
 			console.log(movieBanner);
 			query = "select * from movies where bannerName like "
-					+ connection.escape(movieBanner);
+				+ connection.escape(movieBanner);
 			console.log("SQL search for Movie Banner:" + query);
 
 		} else if (releaseDate) {
 			releaseDate = releaseDate + "%";
 			console.log(releaseDate);
 			query = "select * from movies where releaseDate like "
-					+ connection.escape(releaseDate);
+				+ connection.escape(releaseDate);
 			console.log("SQL search for release Date:" + query);
 
 		} else if (category) {
 			category = category + "%";
 			console.log(category);
 			query = "select * from movies where category like "
-					+ connection.escape(category);
+				+ connection.escape(category);
 			console.log("SQL search for category:" + query);
 
 		} else if (availableCopies) {
 			availableCopies = availableCopies + "%";
 			console.log(availableCopies);
 			query = "select * from movies where availableCopies like "
-					+ connection.escape(availableCopies);
+				+ connection.escape(availableCopies);
 			console.log("SQL search for available copies:" + query);
 
 		} else if (rentAmount) {
 			rentAmount = rentAmount + "%";
 			console.log(rentAmount);
 			query = "select * from movies where rentAmount like "
-					+ connection.escape(rentAmount);
+				+ connection.escape(rentAmount);
 			console.log("SQL search for rent Amount:" + query);
 		}
 
@@ -540,7 +521,7 @@ app.post('/DisplayAllMoviesJSON', function(req, res) {
 });
 
 
-// -----------Search for All Movies------//
+//-----------Search for All Movies------//
 app.post('/DisplayAllMovies', function(req, res) {
 	// Checking the MYSQL connection is available
 	if (connection) {
@@ -576,35 +557,35 @@ app.post('/DisplayAllMovies', function(req, res) {
 			movieBanner = movieBanner + "%";
 			console.log(movieBanner);
 			query = "select * from movies where bannerName like "
-					+ connection.escape(movieBanner);
+				+ connection.escape(movieBanner);
 			console.log("SQL search for Movie Banner:" + query);
 
 		} else if (releaseDate) {
 			releaseDate = releaseDate + "%";
 			console.log(releaseDate);
 			query = "select * from movies where releaseDate like "
-					+ connection.escape(releaseDate);
+				+ connection.escape(releaseDate);
 			console.log("SQL search for release Date:" + query);
 
 		} else if (category) {
 			category = category + "%";
 			console.log(category);
 			query = "select * from movies where category like "
-					+ connection.escape(category);
+				+ connection.escape(category);
 			console.log("SQL search for category:" + query);
 
 		} else if (availableCopies) {
 			availableCopies = availableCopies + "%";
 			console.log(availableCopies);
 			query = "select * from movies where availableCopies like "
-					+ connection.escape(availableCopies);
+				+ connection.escape(availableCopies);
 			console.log("SQL search for available copies:" + query);
 
 		} else if (rentAmount) {
 			rentAmount = rentAmount + "%";
 			console.log(rentAmount);
 			query = "select * from movies where rentAmount like "
-					+ connection.escape(rentAmount);
+				+ connection.escape(rentAmount);
 			console.log("SQL search for rent Amount:" + query);
 		}
 
@@ -643,7 +624,7 @@ app.post('/DisplayAllMovies', function(req, res) {
 
 });
 
-// -----------Search for All Members JSON------//
+//-----------Search for All Members JSON------//
 app.get('/DisplayAllMembersJSON', function(req, res) {
 	// Checking the MYSQL connection is available
 	if (connection) {
@@ -670,7 +651,7 @@ app.get('/DisplayAllMembersJSON', function(req, res) {
 
 });
 
-// -----------Search for All Members------//
+//-----------Search for All Members------//
 app.post('/DisplayAllMembers', function(req, res) {
 	// Checking the MYSQL connection is available
 	if (connection) {
@@ -697,7 +678,7 @@ app.post('/DisplayAllMembers', function(req, res) {
 			console.log("User want to see the Movie Name:" + memType);
 			if (memType !== "-1") {
 				query = "select * from customers where memberType like "
-						+ connection.escape(memType);
+					+ connection.escape(memType);
 
 				memType = memType + "%";
 				console.log(memType);
@@ -712,7 +693,7 @@ app.post('/DisplayAllMembers', function(req, res) {
 			console.log(firstName);
 			// query to match the string input using LIKE in mySQL
 			query = "select * from customers where fname like "
-					+ connection.escape(firstName);
+				+ connection.escape(firstName);
 			// if you get error, copy the line on the console log and check in
 			// your SQL terminal
 			console.log("SQL search for First Name:" + query);
@@ -720,42 +701,42 @@ app.post('/DisplayAllMembers', function(req, res) {
 			lastName = lastName + "%";
 			console.log(lastName);
 			query = "select * from customers where lname like "
-					+ connection.escape(lastName);
+				+ connection.escape(lastName);
 			console.log("SQL search for lastName:" + query);
 
 		} else if (membershipNo) {
 			membershipNo = membershipNo + "%";
 			console.log(membershipNo);
 			query = "select * from customers where membershipno like "
-					+ connection.escape(membershipNo);
+				+ connection.escape(membershipNo);
 			console.log("SQL search for membershipNo:" + query);
 
 		} else if (address) {
 			address = address + "%";
 			console.log(address);
 			query = "select * from customers where address like "
-					+ connection.escape(address);
+				+ connection.escape(address);
 			console.log("SQL search for address:" + query);
 
 		} else if (city) {
 			city = city + "%";
 			console.log(city);
 			query = "select * from customers where city like "
-					+ connection.escape(city);
+				+ connection.escape(city);
 			console.log("SQL search for city:" + query);
 
 		} else if (state) {
 			state = state + "%";
 			console.log(state);
 			query = "select * from customers where state like "
-					+ connection.escape(state);
+				+ connection.escape(state);
 			console.log("SQL search for state:" + query);
 
 		} else if (zipcode) {
 			zipcode = zipcode + "%";
 			console.log(zipcode);
 			query = "select * from customers where zipcode like "
-					+ connection.escape(zipcode);
+				+ connection.escape(zipcode);
 			console.log("SQL search for zipcode:" + query);
 		}
 
